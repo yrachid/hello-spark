8a78a3c1a6b3f390f67f770901e5adf25adb0b76
yrachid <anuar.yasser@gmail.com>
2020-05-07 21:38:30 -0300

    Read unstructured text file

--- src/main/scala/main/HelloSpark.scala
+++ src/main/scala/main/HelloSpark.scala
@@ -5,6 +5,13 @@ import org.apache.spark.sql.SparkSession
 object HelloSpark {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Hello Spark App").getOrCreate()
+
+    val textDf = spark.read.text(s"${System.getProperty("user.home")}/xeroque-holmes.txt")
+
+    textDf.printSchema
+
+    textDf.show
+
     println("Hello Spark")
     spark.stop()
   }
