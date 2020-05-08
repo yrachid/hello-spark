0ca371c0723fc48adb7ee5e520524427255e4737
yrachid <anuar.yasser@gmail.com>
2020-05-07 22:51:54 -0300

    Refactor and read CSV

--- src/main/scala/main/HelloSpark.scala
+++ src/main/scala/main/HelloSpark.scala
@@ -3,16 +3,32 @@ package main
 import org.apache.spark.sql.SparkSession
 
 object HelloSpark {
+
+  private def SampleTextPath = "/tmp/sample.txt"
+
+  private def SampleCsvPath = "/tmp/sample.csv"
+
+  private def readUnstructuredText(spark: SparkSession) = spark
+    .read
+    .text(SampleTextPath)
+
+  private def readCsv(spark: SparkSession) = spark
+    .read
+    .option("header", "true")
+    .csv(SampleCsvPath)
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Hello Spark App").getOrCreate()
 
-    val textDf = spark.read.text(s"${System.getProperty("user.home")}/xeroque-holmes.txt")
+    val textDf = readUnstructuredText(spark)
+    val csvDf = readCsv(spark)
 
-    textDf.printSchema
+    textDf.printSchema()
+    textDf.show(30)
 
-    textDf.show
+    csvDf.printSchema()
+    csvDf.show(30)
 
-    println("Hello Spark")
     spark.stop()
   }
 
