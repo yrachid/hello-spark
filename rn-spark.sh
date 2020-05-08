bafc157e9778cbe36b548fbd1ee184e0ce37b543
yrachid <anuar.yasser@gmail.com>
2020-05-07 22:05:54 -0300

    sbt clean pkg; copy samples to /tmp

--- rn-spark.sh
+++ rn-spark.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sbt assembly && spark-submit \
+cp sample.{csv,txt} /tmp
+
+sbt clean package && spark-submit \
   --master local[*] \
   --class main.HelloSpark \
   target/scala-2.11/data-transformations_2.11-0.1.jar
+
+rm /tmp/sample.{csv,txt}
