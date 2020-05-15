2708a4cbc273bb16b1a9c9c40b25e6ebc183105e
yrachid <anuar.yasser@gmail.com>
2020-05-15 00:24:23 -0300

    Write csv and parquet

--- rn-spark.sh
+++ rn-spark.sh
@@ -5,7 +5,7 @@ cp -r sample.parquet /tmp
 
 sbt clean package && spark-submit \
   --master local[*] \
-  --class main.HelloSpark \
+  --class ${1:-main.HelloSpark} \
   target/scala-2.11/data-transformations_2.11-0.1.jar
 
 rm /tmp/sample.{csv,txt}
---
+++ src/main/scala/writing/SparkWrite.scala
@@ -0,0 +1,28 @@
+package writing
+
+import org.apache.spark.sql.SparkSession
+
+object SparkWrite {
+
+  def main(args: Array[String]): Unit = {
+    val session = SparkSession.builder().appName("spark-csv-write").getOrCreate()
+
+    val weatherDf = session
+      .read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("/tmp/sample.csv")
+
+    weatherDf
+      .write
+      .parquet("/tmp/spark-parquet-output")
+
+    weatherDf
+      .write
+      .option("header", "true")
+      .csv("/tmp/spark-csv-output")
+
+    session.stop()
+  }
+
+}
